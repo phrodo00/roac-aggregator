d306c4e92a4e061e2b284ede1fa1893a818505d1
Javier Aravena Claramunt <javier@aravenas.com>
2013-10-19 02:51:17 -0300

    Display node details.

--- roac_aggregator/static/js/nodes.js
+++ roac_aggregator/static/js/nodes.js
@@ -1,21 +1,38 @@
 $(function() {
-  function NodesModel() {
-    var self = this;
-    self.nodes = ko.observableArray([]);
-    self.active_node = ko.observable();
-    
-    self.update_nodes = function() {
-      $.getJSON("/api/v1/nodes", function(data) {
-        self.nodes(data);
-        self.change_node(self.nodes()[0])
-      });
-    };
+    function Node(name, status_) {
+        var self = this;
+        self.name = ko.observable(name);
+        self.status_ = ko.observable(status_);
+        self.status_text = ko.computed(function() {
+            return JSON.stringify(self.status_(), undefined, 2);
+        });
+    }
 
-    self change_node = function(node) {
-      self.active_node(node)
-    };
+    function NodesModel() {
+        var self = this;
+        self.nodes = ko.observableArray([]);
+        self.selected_node = ko.observable();
+        self.node = ko.observable();
 
-    self.update_nodes()
-  }
-  ko.applyBindings(new NodesModel());
+        self.update_nodes = function() {
+            $.getJSON("/api/v1/nodes", function(data) {
+                self.nodes(data);
+                self.change_node(self.nodes()[0])
+            });
+        };
+
+
+        self.change_node = function(node) {
+            self.selected_node(node)
+            $.getJSON("/api/v1/nodes/" + node, function(data) {
+                var node = new Node(data["name"], data["status"]);
+                self.node(node);
+                console.log(node.status_text());
+            });
+        };
+
+        self.update_nodes()
+    }
+
+    ko.applyBindings(new NodesModel());
 });
