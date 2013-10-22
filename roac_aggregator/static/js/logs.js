877d64956908d6c7a5b7f88e8c03a2b0216058d7
Javier Aravena Claramunt <javier@aravenas.com>
2013-10-22 02:09:46 -0300

    Show logs times and nodes.

---
+++ roac_aggregator/static/js/logs.js
@@ -0,0 +1,40 @@
+$(function() {
+  function Record(name, created_at, results) {
+    var self = this;
+    self.name = ko.observable(name);
+    self.created_at = ko.observable(created_at);
+    self.results = ko.observableArray(results);
+    self.formated_date = ko.computed(function() {
+      date = self.created_at().getDate();
+      month = self.created_at().getMonth() + 1;
+      year = self.created_at().getFullYear();
+      return year + '-' + month + '-' + date;
+    });
+    self.formated_time = ko.computed(function() {
+      seconds = self.created_at().getSeconds();
+      minutes = self.created_at().getMinutes();
+      hours = self.created_at().getHours();
+      return hours + ':' + minutes + ':' + seconds;
+    });
+  }
+
+  function LogModel() {
+    self.log = ko.observableArray([]);
+
+    self.populate_log = function() {
+      //replaces the content of the log by updated info.
+      $.getJSON("/api/v1/logs", function(data) {
+        var mappedRecords = $.map(data, function(record) {
+          created_at = new Date(Date.parse(record.created_at));
+          return new Record(record.name, created_at, record.results);
+        });
+        self.log(mappedRecords)
+      });
+    };
+
+    self.populate_log();
+  }
+
+  ko.applyBindings(new LogModel());
+});
+
