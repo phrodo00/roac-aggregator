82baefbc28b61468e1055742514bbaa3c83eda48
Javier Aravena Claramunt <javier@aravenas.com>
2013-10-24 05:16:41 -0300

    Show record detail in log view.

--- roac_aggregator/static/js/logs.js
+++ roac_aggregator/static/js/logs.js
@@ -22,6 +22,10 @@ $(function() {
 
   function LogModel() {
     self.log = ko.observableArray([]);
+    self.info = ko.observable();
+    self.info_text = ko.computed(function() {
+        return JSON.stringify(self.info(), undefined, 2);
+    });
 
     self.populate_log = function() {
       //replaces the content of the log by updated info.
@@ -37,6 +41,12 @@ $(function() {
     self.repeat_populate_log = function() {
       self.populate_log();
       setTimeout(function() {self.repeat_populate_log();}, 1000);
+    }
+
+    self.show_info = function(record) { 
+      //requires a bootstrap modal called #modal that displays info_text
+      self.info(record.results());
+      $('#modal').modal();
     }
 
     self.repeat_populate_log();
