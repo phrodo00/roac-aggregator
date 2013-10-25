d031e6b1158967407c709820fa86e90b7a9ea468
Javier Aravena Claramunt <javier@aravenas.com>
2013-10-25 03:09:27 -0300

    Settled configuration management.

--- roac_aggregator/__init__.py
+++ roac_aggregator/__init__.py
@@ -4,13 +4,11 @@ from .json import JSONEncoder
 from .mongodb import MongoDB
 
 
-app = Flask(__name__.split('.')[0])
+app = Flask(__name__.split('.')[0], instance_relative_config=True)
 
-SECRET_KEY = "development key"
-DEBUG = True
-MONGO_DBNAME = 'roac'
-
-app.config.from_object(__name__)
+app.config.from_object('roac_aggregator.defaults')
+app.config.from_pyfile('roacagg.cfg', silent=True)
+app.config.from_envvar('ROACAGG_SETTINGS', silent=True)
 
 
 app.json_encoder = JSONEncoder
--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 from roac_aggregator import app
 
+app.debug = True
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
