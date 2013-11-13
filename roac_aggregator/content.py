88c03121dd08eaa857ca29781cc975edcb9c37ba
Javier Aravena Claramunt <javier@aravenas.com>
2013-11-13 01:05:42 -0300

    First page shows nodes
    It was logs.

--- roac_aggregator/content.py
+++ roac_aggregator/content.py
@@ -1,16 +1,21 @@
 from __future__ import absolute_import
-from flask import render_template
+from flask import render_template, redirect, url_for
 from . import app
 
 
-@app.route('/nodes')
+@app.route('/')
 def nodes():
     return render_template('nodes.html')
 
 
-@app.route('/')
+@app.route('/nodes')
+def redirect_to_index():
+    return redirect(url_for(nodes.__name__))
+
+
+@app.route('/log')
 def logs():
-    return render_template('index.html')
+    return render_template('log.html')
 
 
 @app.route('/alarms')
